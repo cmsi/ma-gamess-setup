67700243f7faf5e57b7ef828c65c5d2588f85841
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2017-05-08 00:15:04 +0900

    Update for gamess version 201704

--- gamess-setup/compile.sh
+++ gamess-setup/compile.sh
@@ -31,6 +31,10 @@ if [ "$MD5SUM" = "977a01a8958238c67b707f125999fcec" ]; then
   VERSION="201305"
 elif [ "$MD5SUM" = "6403592eaa885cb3691505964d684516" ]; then
   VERSION="201412"
+elif [ "$MD5SUM" = "47ef3161ab148acf98a118997825f9a0" ]; then
+  VERSION="201608"
+elif [ "$MD5SUM" = "e9fa2e725ccbd69f4d996ab68bb65ff2" ]; then
+  VERSION="201704"
 else
   echo "Error: unkown version or corrupted archive"
   exit 127
@@ -40,18 +44,32 @@ mkdir -p "$SHAREDIR"
 echo "Extracting $SOURCE into $SHAREDIR"
 tar zxvf "$SOURCE" -C "$SHAREDIR"
 
+case "$(dpkg --print-architecture)" in
+        amd64)
+                _TARGET="linux64"
+                ;;
+
+        i386)
+                _TARGET="linux32"
+                ;;
+esac
+
 # Generate config file
 echo "Generating $GAMESSDIR/install.info"
 cat << EOF > "$GAMESSDIR/install.info"
 setenv GMS_PATH $GAMESSDIR
 setenv GMS_BUILD_DIR $GAMESSDIR
-setenv GMS_TARGET linux64
+setenv GMS_TARGET $_TARGET
 setenv GMS_FORTRAN gfortran
 setenv GMS_GFORTRAN_VERNO 4.7
-setenv GMS_MATHLIB atlas
+setenv GMS_MATHLIB none
 setenv GMS_MATHLIB_PATH /usr/lib
 setenv GMS_DDI_COMM sockets
 setenv GMS_LIBCCHEM false
+setenv GMS_EIGEN_PATH
+setenv GMS_PHI false
+setenv GMS_SHMTYPE sysv
+setenv GMS_OPENMP false
 EOF
 echo "Generating $GAMESSDIR/Makefile"
 cat << EOF > "$GAMESSDIR/Makefile"
