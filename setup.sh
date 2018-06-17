431431c38124e4d1c2515b3fbdadfa903ac5a56d
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2018-06-17 10:56:56 +0900

    Merge debian7 and later

--- setup.sh
+++ setup.sh
@@ -7,7 +7,4 @@ set -x
 
 mkdir -p $BUILD_DIR
 cp -frp $SCRIPT_DIR/debian $BUILD_DIR
-if test $(lsb_release -c -s) = "wheezy"; then
-  cp -fp $SCRIPT_DIR/debian7/* $BUILD_DIR/debian
-fi
 cp -frp $SCRIPT_DIR/files/* $BUILD_DIR
